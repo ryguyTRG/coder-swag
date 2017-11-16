58fe4e9cd71ef2fb024c9b84e6c3cde2ede8b425
Ryan Gjoraas <ryangjoraas@Ryans-MacBook-Pro.local>
2017-11-15 22:13:48 -0600

    lesson-02 finished

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ryan Gjoraas on 11/15/17.
+//  Copyright © 2017 Gorgeous Running. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
